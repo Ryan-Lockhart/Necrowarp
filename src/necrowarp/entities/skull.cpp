a924ea0a69bbdfb8d95cab7534d3c5e195cde7de
Ryan Lockhart <ryanjeffreylockhart@gmail.com>
2025-04-01 20:58:08 -0400

    initial commit, this repository will be for both windows and linxu build unlike previously planned

---
+++ src/necrowarp/entities/skull.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <necrowarp/entities/skull.hpp>
+
+#include <necrowarp/entity_state.hpp>
+#include <necrowarp/entity_state.cpp>
+
+namespace necrowarp {} // namespace necrowarp
