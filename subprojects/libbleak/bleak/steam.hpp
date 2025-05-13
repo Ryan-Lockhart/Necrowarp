27d715db4dd86814f9ee23494f6891d907d19d90
Ryan Lockhart <ryanjeffreylockhart@gmail.com>
2025-05-12 21:05:35 -0400

    reverted git submodule divergence shenaniganry

--- subprojects/libbleak/bleak/steam.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// IWYU pragma: begin_exports
-#include <bleak/steam/api.hpp>
-#include <bleak/steam/input.hpp>
-#include <bleak/steam/user.hpp>
-#include <bleak/steam/user_stats.hpp>
-// IWYU pragma: end_exports
