318714acead80d0ac5c6928d2b0404c8f2564b7e
Ryan Lockhart <ryanjeffreylockhart@gmail.com>
2025-05-14 20:27:33 -0400

    cut most of the unneeded properties of cell.hpp, left in visibility stuff because that might be good for a hardmode

--- inc/necrowarp/cell.hpp
+++ inc/necrowarp/cell.hpp
@@ -4,7 +4,6 @@
 
 #include <bit>
 #include <format>
-#include <random>
 #include <string>
 
 namespace necrowarp {
@@ -19,18 +18,10 @@ namespace necrowarp {
 		Seen,
 		Unexplored,
 		Explored,
-		Dry,
-		Damp,
-		Cold,
-		Warm,
 		Rough,
 		Smooth,
 		Recedes,
-		Protrudes,
-		Clean,
-		Bloodied,
-		Safe,
-		Toxic
+		Protrudes
 	};
 
 	constexpr cstr to_string(cell_trait_t trait) noexcept {
@@ -51,14 +42,6 @@ namespace necrowarp {
 				return "unexplored";
 			} case cell_trait_t::Explored: {
 				return "explored";
-			} case cell_trait_t::Dry: {
-				return "dry";
-			} case cell_trait_t::Damp: {
-				return "damp";
-			} case cell_trait_t::Cold: {
-				return "cold";
-			} case cell_trait_t::Warm: {
-				return "warm";
 			} case cell_trait_t::Rough: {
 				return "rough";
 			} case cell_trait_t::Smooth: {
@@ -67,92 +50,6 @@ namespace necrowarp {
 				return "recedes";
 			} case cell_trait_t::Protrudes: {
 				return "protrudes";
-			} case cell_trait_t::Clean: {
-				return "clean";
-			} case cell_trait_t::Bloodied: {
-				return "bloodied";
-			} case cell_trait_t::Safe: {
-				return "safe";
-			} case cell_trait_t::Toxic: {
-				return "toxic";
-			}
-		}
-	}
-
-	enum class rock_type_t : u8 {
-		Limestone, // a sedimentary stone
-		Granite,   // an igneous intrusive stone
-		Basalt,	   // an igneous extrusive stone
-		Marble	   // a metamorphic stone
-	};
-
-	constexpr cstr to_string(rock_type_t type) noexcept {
-		switch (type) {
-			case rock_type_t::Limestone: {
-				return "limestone";
-			} case rock_type_t::Granite: {
-				return "granite";
-			} case rock_type_t::Basalt: {
-				return "basalt";
-			} case rock_type_t::Marble: {
-				return "marble";
-			}
-		}
-	}
-
-	enum class mineral_type_t : u8 {
-		None,
-		Lignite,		// a low yield mineral of coke
-		BituminousCoal, // a medium yield mineral of coke
-		NativeCopper,	// a native deposit of copper
-		NativeSilver,	// a native deposit of silver
-		NativeGold,		// a native deposit of gold
-		NativePlatinum, // a native deposit of platinum
-		Limonite,		// a low yield ore of iron
-		Hematite,		// a medium yield ore of iron
-		Magnetite,		// a high yield ore of iron
-		Malachite,		// an ore of copper
-		Tetrahedrite,	// an ore of copper and silver (5:1)
-		Garnierite,		// an ore of nickel
-		Galena,			// an ore of lead and silver (2:1)
-		Sphalerite,		// an ore of zinc
-		Cassiterite		// an ore of tin
-	};
-
-	constexpr cstr to_string(mineral_type_t type) noexcept {
-		switch (type) {
-			case mineral_type_t::None: {
-				return "no minerals";
-			} case mineral_type_t::Lignite: {
-				return "lignite";
-			} case mineral_type_t::BituminousCoal: {
-				return "bituminous coal";
-			} case mineral_type_t::NativeCopper: {
-				return "native copper";
-			} case mineral_type_t::NativeSilver: {
-				return "native silver";
-			} case mineral_type_t::NativeGold: {
-				return "native gold";
-			} case mineral_type_t::NativePlatinum: {
-				return "native platinum";
-			} case mineral_type_t::Limonite: {
-				return "limonite";
-			} case mineral_type_t::Hematite: {
-				return "hematite";
-			} case mineral_type_t::Magnetite: {
-				return "magnetite";
-			} case mineral_type_t::Malachite: {
-				return "malachite";
-			} case mineral_type_t::Tetrahedrite: {
-				return "tetrahedrite";
-			} case mineral_type_t::Garnierite: {
-				return "garnierite";
-			} case mineral_type_t::Galena: {
-				return "galena";
-			} case mineral_type_t::Sphalerite: {
-				return "sphalerite";
-			} case mineral_type_t::Cassiterite: {
-				return "cassiterite";
 			}
 		}
 	}
@@ -163,16 +60,9 @@ namespace necrowarp {
 		bool opaque : 1 { false };
 		bool seen : 1 { false };
 		bool explored : 1 { false };
-		bool damp : 1 { false };
-		bool warm : 1 { false };
 		bool smooth : 1 { false };
 		bool protrudes : 1 { false };
-		bool bloodied : 1 { false };
-		bool toxic : 1 { false };
-
-		rock_type_t rock_type : 2 { rock_type_t::Limestone };
-
-		mineral_type_t mineral_type : 4 { mineral_type_t::None };
+		bool : 2;
 
 		constexpr cell_state_t() noexcept = default;
 
@@ -246,18 +136,6 @@ namespace necrowarp {
 			case cell_trait_t::Explored:
 				explored = true;
 				break;
-			case cell_trait_t::Dry:
-				damp = false;
-				break;
-			case cell_trait_t::Damp:
-				damp = true;
-				break;
-			case cell_trait_t::Cold:
-				warm = false;
-				break;
-			case cell_trait_t::Warm:
-				warm = true;
-				break;
 			case cell_trait_t::Rough:
 				smooth = false;
 				break;
@@ -269,18 +147,6 @@ namespace necrowarp {
 				break;
 			case cell_trait_t::Protrudes:
 				protrudes = true;
-				break;
-			case cell_trait_t::Clean:
-				bloodied = false;
-				break;
-			case cell_trait_t::Bloodied:
-				bloodied = true;
-				break;
-			case cell_trait_t::Safe:
-				toxic = false;
-				break;
-			case cell_trait_t::Toxic:
-				toxic = true;
 				break;
 			default:
 				break;
@@ -313,18 +179,6 @@ namespace necrowarp {
 			case cell_trait_t::Explored:
 				explored = false;
 				break;
-			case cell_trait_t::Dry:
-				damp = true;
-				break;
-			case cell_trait_t::Damp:
-				damp = false;
-				break;
-			case cell_trait_t::Cold:
-				warm = true;
-				break;
-			case cell_trait_t::Warm:
-				warm = false;
-				break;
 			case cell_trait_t::Rough:
 				smooth = true;
 				break;
@@ -337,49 +191,23 @@ namespace necrowarp {
 			case cell_trait_t::Recedes:
 				protrudes = false;
 				break;
-			case cell_trait_t::Clean:
-				bloodied = true;
-				break;
-			case cell_trait_t::Bloodied:
-				bloodied = false;
-				break;
-			case cell_trait_t::Safe:
-				toxic = true;
-				break;
-			case cell_trait_t::Toxic:
-				toxic = false;
-				break;
 			default:
 				break;
 			}
 		}
 
-		constexpr ref<cell_state_t> operator=(cref<cell_trait_t> trait) noexcept {
+		constexpr ref<cell_state_t> operator=(cell_trait_t trait) noexcept {
 			set(trait);
 
 			return *this;
 		}
 
-		constexpr ref<cell_state_t> operator=(cref<rock_type_t> type) noexcept {
-			rock_type = type;
-
-			return *this;
-		}
-
-		constexpr ref<cell_state_t> operator=(cref<mineral_type_t> type) noexcept {
-			mineral_type = type;
-
-			return *this;
-		}
-
 		constexpr bool operator==(cref<cell_state_t> other) const noexcept {
-			return solid == other.solid && opaque == other.opaque && seen == other.seen && explored == other.explored && damp == other.damp && warm == other.warm && smooth == other.smooth && protrudes == other.protrudes
-				&& bloodied == other.bloodied && toxic == other.toxic && rock_type == other.rock_type && mineral_type == other.mineral_type;
+			return solid == other.solid && opaque == other.opaque && seen == other.seen && explored == other.explored && smooth == other.smooth && protrudes == other.protrudes;
 		}
 
 		constexpr bool operator!=(cref<cell_state_t> other) const noexcept {
-			return solid != other.solid || opaque != other.opaque || seen != other.seen || explored != other.explored || damp != other.damp || warm != other.warm || smooth != other.smooth || protrudes != other.protrudes
-				|| bloodied != other.bloodied || toxic != other.toxic || rock_type != other.rock_type || mineral_type != other.mineral_type;
+			return solid != other.solid || opaque != other.opaque || seen != other.seen || explored != other.explored || smooth != other.smooth || protrudes != other.protrudes;
 		}
 
 		constexpr bool operator==(cref<cell_trait_t> other) const noexcept { return contains(other); }
@@ -404,14 +232,6 @@ namespace necrowarp {
 				return explored;
 			case cell_trait_t::Unexplored:
 				return !explored;
-			case cell_trait_t::Damp:
-				return damp;
-			case cell_trait_t::Dry:
-				return !damp;
-			case cell_trait_t::Warm:
-				return warm;
-			case cell_trait_t::Cold:
-				return !warm;
 			case cell_trait_t::Smooth:
 				return smooth;
 			case cell_trait_t::Rough:
@@ -420,14 +240,6 @@ namespace necrowarp {
 				return protrudes;
 			case cell_trait_t::Recedes:
 				return !protrudes;
-			case cell_trait_t::Bloodied:
-				return bloodied;
-			case cell_trait_t::Clean:
-				return !bloodied;
-			case cell_trait_t::Toxic:
-				return toxic;
-			case cell_trait_t::Safe:
-				return !toxic;
 			default:
 				return false;
 			}
@@ -436,21 +248,14 @@ namespace necrowarp {
 		inline constexpr std::string to_tooltip() const {
 			if (seen && explored) {
 				return std::format(
-					"The cell is physically {} and visibility is {}.\nIt is {} and {} by the player.\nThe {}{} {} is {}, {}, and {} to the touch.{}{}",
+					"The cell is physically {} and visibility is {}.\nIt is {} and {} by the player.\nThe {}{} is {} to the touch.",
 					solid ? "blocked" : "open",
 					opaque ? "obscured" : "unobscured",
 					seen ? "in view" : "not in view",
 					explored ? "was explored" : "remains unexplored",
 					solid ? protrudes ? "protruding " : "receding " : "",
-					to_string(rock_type),
 					solid ? "wall" : "floor",
-					damp ? "damp" : "dry",
-					warm ? "warm" : "cold",
-					smooth ? "smooth" : "rough",
-					std::format(
-						"\nIts surface is {}{}",
-						bloodied ? "spattered with blood" : "clean", mineral_type == mineral_type_t::None || !solid ? "." : std::format(" and encrusted with {}.", to_string(mineral_type))),
-					solid ? "" : std::format("\nThe air within is {}.", toxic ? "toxic" : "innocuous")
+					smooth ? "smooth" : "rough"
 				);
 			} else if (explored) {
 				return std::format(
@@ -467,51 +272,15 @@ namespace necrowarp {
 
 		inline constexpr operator std::string() const {
 			return std::format(
-				"[{}, {}, {}, {}, {}, {}, {}, {}, {}]",
+				"[{}, {}, {}, {}, {}, {}]",
 				solid ? "Solid" : "Open",
 				opaque ? "Opaque" : "Transperant",
 				seen ? "Seen" : "Unseen",
 				explored ? "Explored" : "Unexplored",
-				damp ? "Damp" : "Dry",
-				warm ? "Warm" : "Cold",
 				smooth ? "Smooth" : "Rough",
-				protrudes ? "Protrudes" : "Recedes",
-				bloodied ? "Bloodied" : "Clean",
-				toxic ? "Toxic" : "Safe"
+				protrudes ? "Protrudes" : "Recedes"
 			);
 		}
-
-		/*template<extent_t AtlasSize> inline constexpr void draw(cref<atlas_t<AtlasSize>> atlas, offset_t position) const noexcept {
-			if (!explored) {
-				return;
-			}
-
-			const u8 alpha{ seen ? u8{ 0xFF } : u8{ 0x80 } };
-
-			atlas.draw(glyph_t{ characters::Floor, color_t{ 0x40, alpha } }, position);
-
-			if (!solid) {
-				return;
-			}
-
-			atlas.draw(glyph_t{ characters::Wall, color_t{ 0xC0, alpha } }, position);
-		}
-
-		template<extent_t AtlasSize> inline constexpr void draw(cref<atlas_t<AtlasSize>> atlas, offset_t position, offset_t offset) const noexcept {
-			if (!explored) {
-				return;
-			}
-
-			const u8 alpha{ seen ? u8{ 0xFF } : u8{ 0x80 } };
-
-			atlas.draw(glyph_t{ characters::Floor, color_t{ 0x40, alpha } }, position + offset);
-
-			if (!solid) {
-				return;
-			}
-
-			atlas.draw(glyph_t{ characters::Wall, color_t{ 0xC0, alpha } }, position + offset);
-		}*/
 
 		template<extent_t AtlasSize, typename T, extent_t ZoneSize, extent_t ZoneBorder>
 		inline constexpr void draw(cref<atlas_t<AtlasSize>> atlas, cref<zone_t<T, ZoneSize, ZoneBorder>> zone, offset_t position) const noexcept {
@@ -521,7 +290,7 @@ namespace necrowarp {
 
 			const u8 alpha{ seen ? u8{ 0xFF } : u8{ 0x80 } };
 
-			atlas.draw(glyph_t{ characters::Floor, bloodied ? seen ? colors::materials::LightBlood : colors::materials::DarkBlood : color_t{ 0x40, alpha } }, position);
+			atlas.draw(glyph_t{ characters::Floor, color_t{ 0x40, alpha } }, position);
 
 			if (!solid) {
 				return;
@@ -540,7 +309,7 @@ namespace necrowarp {
 
 			const u8 alpha{ seen ? u8{ 0xFF } : u8{ 0x80 } };
 
-			atlas.draw(glyph_t{ characters::Floor, bloodied ? seen ? colors::materials::LightBlood : colors::materials::DarkBlood : color_t{ 0x40, alpha } }, position + offset);
+			atlas.draw(glyph_t{ characters::Floor, color_t{ 0x40, alpha } }, position + offset);
 
 			if (!solid) {
 				return;
@@ -559,7 +328,7 @@ namespace necrowarp {
 
 			const u8 alpha{ seen ? u8{ 0xFF } : u8{ 0x80 } };
 
-			atlas.draw(glyph_t{ characters::Floor, bloodied ? seen ? colors::materials::LightBlood : colors::materials::DarkBlood : color_t{ 0x40, alpha } }, position + offset, nudge);
+			atlas.draw(glyph_t{ characters::Floor, color_t{ 0x40, alpha } }, position + offset, nudge);
 
 			if (!solid) {
 				return;
@@ -571,23 +340,7 @@ namespace necrowarp {
 		}
 
 		struct hasher {
-			static constexpr usize operator()(cref<cell_state_t> cell_state) noexcept { return hash_combine(std::bit_cast<u16>(cell_state)); }
-		};
-
-		template<RandomEngine Generator> struct randomizer {
-			template<typename T> static constexpr T operator()(ref<Generator> generator) noexcept {
-				if constexpr (std::is_same<T, rock_type_t>::value) {
-					static std::uniform_int_distribution<i32> dis{ static_cast<i32>(rock_type_t::Limestone), static_cast<i32>(rock_type_t::Marble) };
-
-					return static_cast<rock_type_t>(dis(generator));
-				} else if constexpr (std::is_same<T, mineral_type_t>::value) {
-					static std::uniform_int_distribution<i32> dis{ static_cast<i32>(mineral_type_t::None), static_cast<i32>(mineral_type_t::Tetrahedrite) };
-
-					return static_cast<mineral_type_t>(dis(generator));
-				}
-
-				return T{};
-			}
+			static constexpr usize operator()(cref<cell_state_t> cell_state) noexcept { return hash_combine(std::bit_cast<u8>(cell_state)); }
 		};
 	};
 } // namespace necrowarp
