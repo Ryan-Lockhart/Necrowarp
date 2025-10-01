5096c8cf939fc0d139155726d2887284fdab6362
Ryan Lockhart <ryanjeffreylockhart@gmail.com>
2025-09-30 20:47:29 -0400

    fuffed around with tribulation trap triggering

--- inc/necrowarp/tribulation.hpp
+++ inc/necrowarp/tribulation.hpp
@@ -7,14 +7,14 @@ namespace necrowarp {
 
 	struct tribulation_s {
 	  private:
-		static constexpr usize MinimumTrapChance{ 0'125 };
-		static constexpr usize MaximumTrapChance{ 0'500 };
+		static constexpr f16 MinimumTrapChance{ 0.0125f };
+		static constexpr f16 MaximumTrapChance{ 0.5000f };
 
 		static constexpr f16 TrapGrowthRate{ 2.0f };
 
 		static inline usize trap_aggravations{ 0 };
 
-		static inline f16 current_trap_chance() noexcept { return clamp<f16>(static_cast<f16>(trap_aggravations * MinimumTrapChance * TrapGrowthRate / 1000.0f), MinimumTrapChance / 1000.0f, MaximumTrapChance / 1000.0f); }
+		static inline f16 current_trap_chance() noexcept { return clamp<f16>(static_cast<f16>(trap_aggravations * MinimumTrapChance * TrapGrowthRate), MinimumTrapChance, MaximumTrapChance); }
 
 		static inline std::bernoulli_distribution trap_dis{ current_trap_chance() };
 
